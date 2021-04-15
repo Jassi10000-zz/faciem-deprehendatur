aa09e0f2d993ea8cd7ccb4094a22c5159acb643d
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-04-16 00:45:18 +0530

    added runFaceMesh, detect function

--- src/App.js
+++ src/App.js
@@ -11,7 +11,58 @@ function App() {
   const jsCanvasRef = useRef(null);
 
 
+  // load facemesh
+  const runFaceMesh = async () => {
 
+    // this will load a neural network from tf.jsz
+    const network = await facemesh.load({
+      inputResolution: {width: 640 , height: 480 }, scale: 0.8
+    });
+    // runFaceMesh function will run after every 100 ms to detect 3D points and create a mask
+    setInterval(() =>{
+        detect(network);
+    },100)
+
+  };
+
+
+
+  //Detect function
+  const detect = async (net) => {
+    if(
+      webcamRef.current !== undefined &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ){
+
+      // get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+
+      //set video's width and height
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+
+       //set canvas's width and height
+       jsCanvasRef.current.width = videoWidth;
+       jsCanvasRef.current.height = videoHeight;
+
+
+       //make detections
+       //net.estimateFaces will receive our network or our neural networks from tensorflow
+       //and thus estimateFaces method allows us to detect all of our facial landmarks
+       const detectFace = await net.estimateFaces(video);
+       console.log(detectFace);
+
+       // 
+
+    }
+  };
+
+  runFaceMesh();
 
   return (
     <div className="App">
@@ -26,7 +77,7 @@ function App() {
           right:0,
           textAlign: "center",
           zIndex: 9,
-          width: 600,
+          width: 640,
           height: 480
         }
       }/>
@@ -40,7 +91,7 @@ function App() {
           right:0,
           textAlign: "center",
           zIndex: 9,
-          width: 600,
+          width: 640,
           height: 480
         }
       }/>
