3c627016cc839f6a592a03ffd5da2f02315c20bd
JASKEERAT SINGH <jaskeerat10000@gmail.com>
2021-04-15 22:44:08 +0530

    webcam and canvas set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React , {useRef} from 'react';
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+import * as facemesh from '@tensorflow-models/facemesh';
+import Webcam from "react-webcam";
 
 function App() {
+
+  // setup references for React Webcam and JavaScript Canvas
+  const webcamRef = useRef(null);
+  const jsCanvasRef = useRef(null);
+
+
+
+
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Webcam ref={webcamRef} style={
+        {
+          position: 'absolute',
+          marginLeft: "auto",
+          marginRight: "auto",
+          left:0,
+          right:0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 600,
+          height: 480
+        }
+      }/>
+
+      <canvas ref={jsCanvasRef} style={
+        {
+          position: 'absolute',
+          marginLeft: "auto",
+          marginRight: "auto",
+          left:0,
+          right:0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 600,
+          height: 480
+        }
+      }/>
       </header>
+
     </div>
   );
 }
